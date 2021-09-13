e63d77b049093f9a9ed78c5bc4551945f3a0b8cc
곰 <@DESKTOP-8D09HCR>
2021-09-13 21:29:13 +0900

    김우빈 코드변경

---
+++ text/src/text/Car.java
@@ -0,0 +1,22 @@
+package text;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Car extends Canvas {
+	public int x = 0;
+	public int y = 0;
+	public BufferedImage image;
+	
+	public Car(int x, int y) {
+		this.x = x; this.y = y;
+		try {
+			image = ImageIO.read(new File("src/text/car.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
